4042d36eb7b5d0fff0b82bcd0668ad1de4f32e05
Akshaykumar <akshaykumarpatil33@gmail.com>
2024-03-13 13:27:54 +0530

    final commit

--- xstates/src/App.jsx
+++ xstates/src/App.jsx
@@ -1,20 +1,93 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [states, setStates] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [city, setCity] = useState('');
 
+  useEffect(() => {
+    fetch('https://crio-location-selector.onrender.com/countries')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch countries');
+        }
+        return res.json();
+      })
+      .then((data) => setCountries(data))
+      .catch((err) => console.log("Error in fetching countries", err));
+  
+    if (country) {
+      fetch(`https://crio-location-selector.onrender.com/country=${country}/states`)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Failed to fetch states');
+          }
+          return res.json();
+        })
+        .then((data) => setStates(data))
+        .catch((err) => console.log("Error in fetching states", err));
+  
+      if (state) {
+        fetch(`https://crio-location-selector.onrender.com/country=${country}/state=${state}/cities`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error('Failed to fetch cities');
+            }
+            return res.json();
+          })
+          .then((data) => setCities(data))
+          .catch((err) => console.log("Error in fetching cities", err));
+      }
+    }
+    
+  }, [country, state, city]);
+  
+
+  const handleSelectChange = (e) => {
+    const { name, value } = e.target;
+     if (name === 'city') setCity(value);
+    if (name === 'country') setCountry(value);
+    if (name === 'state') setState(value);
+  };
+  
 
   return (
     <>
-      
       <div className="card">
-       
+      <select value={country} name='country' onChange={handleSelectChange}>
+  <option value="">Select a Country</option>
+  {countries.map((option) => (
+    <option  key={option} value={option}>{option}</option>
+  ))}
+</select>
+<select value={state} name='state' onChange={handleSelectChange} disabled={!country}>
+  <option value="">Select a State</option>
+  {states.map((option) => (
+    <option key={option} value={option}>{option}</option>
+  ))}
+</select>
+<select value={city} name='city' onChange={handleSelectChange} disabled={!state}>
+  <option value="">Select a City</option>
+  {cities.map((option) => (
+    <option key={option} value={option}>{option}</option>
+  ))}
+</select>
+
       </div>
-     
+      {(city && state && country) && (
+      <>
+      <span>You selected </span>
+      <span className='country'>{country},</span>
+      <span className='cityandstate'>{state},{city}</span>
+      </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
